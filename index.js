789203a0a334779382de0f18f71c869a44695d49
ZY <1822545638@qq.com>
2023-12-15 01:03:25 +0800

    http/1.0设置协商缓存---etag/if-none-match

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const server = http.createServer((request, resp) => {
     // resp.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
@@ -17,7 +18,21 @@ const server = http.createServer((request, resp) => {
     // // 会忽略Expires的设置
     // resp.setHeader('Cache-Control', 'max-age=40');
 
-    // http/1.1设置协商缓存:Last-Modified/If-Modified-Since
+    // http/1.0设置协商缓存---etag/if-none-match
+    const buffer = fs.readFileSync('./dist/index.html'); // 二进制文件流
+    const hashTool = crypto.createHash('md5'); // 使用md5加密算法
+    hashTool.update(buffer, 'utf-8'); // 注入二进制文件
+    const md5 = hashTool.digest('hex'); // 生成md5加密的唯一标识
+
+    const requestEtag = request.headers['if-none-match'];
+    if(requestEtag && requestEtag == md5) {
+        console.log('走浏览器缓存--etag');
+        resp.statusCode = 304;
+        resp.end();
+        return;
+    }
+
+    // http/1.0设置协商缓存:Last-Modified/If-Modified-Since
     const requestModified = request.headers['if-modified-since'];
     const info = fs.statSync('./dist/index.html');
     const lastModified = info.mtime.toUTCString();
@@ -31,6 +46,7 @@ const server = http.createServer((request, resp) => {
 
     console.log('不取缓存');
     resp.setHeader('Last-Modified', lastModified);
+    resp.setHeader('etag', md5);
     const html = fs.readFileSync('./dist/index.html', 'utf-8');
     resp.end(html);
 });
--- test.js
+++ test.js
@@ -1,4 +1,15 @@
+// const fs = require('fs');
+
+// const info = fs.statSync('./dist/index.html');
+// console.log(info);
+
+const crypto = require('crypto');
 const fs = require('fs');
 
-const info = fs.statSync('./dist/index.html');
-console.log(info);
+const buffer = fs.readFileSync('./dist/index.html'); // 二进制文件流
+const hashTool = crypto.createHash('md5'); // 使用md5加密算法
+
+hashTool.update(buffer, 'utf-8'); // 注入二进制文件
+const md5 = hashTool.digest('hex'); // 生成md5加密的唯一标识
+
+console.log(md5);
